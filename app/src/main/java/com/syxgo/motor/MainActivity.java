e235d3a7de410260abb8b77e9f144f6021d0edc2
ruichengrui <ruichengrui@syxgo.com>
2017-03-12 14:18:26 +0800

    hotfix

--- app/src/main/java/com/syxgo/motor/MainActivity.java
+++ app/src/main/java/com/syxgo/motor/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //测试git   33333
+        //测试git 本地修改
     }
 }
